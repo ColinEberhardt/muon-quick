6357860a0216eb8fc4386d1a925ed0144a51c9a2
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2017-01-30 16:28:20 +0000

    renderer accessing node APIs

--- main.js
+++ main.js
@@ -19,7 +19,11 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadURL('https://www.brave.com')
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const fs = require('fs')
+
+fs.readdir('c:', (err, files) => {
+  files.forEach(file => {
+    document.body.innerHTML += '<br/>' + file
+  });
+})
